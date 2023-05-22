f9864a30720cb5f1e15cbe88b2b300a58d4e425d
ariaan ahmed <you@example.com>
2023-05-22 16:56:07 +0600

    search button and functionality added

--- src/pages/home/services/Services.jsx
+++ src/pages/home/services/Services.jsx
@@ -1,20 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
 
     const [services, setServices] = useState([]);
     const [asc, setAsc] = useState(true);
-
+    const searchRef = useRef(null);
+    const [search, setSearch] = useState('')
 
     useEffect(() => {
-        fetch(`http://localhost:5000/services?sort=${asc ? 'asc' : 'des'}`)
+        fetch(`http://localhost:5000/services?search=${search}&sort=${asc ? 'asc' : 'des'}`)
             .then((res) => res.json())
             .then((data) => setServices(data))
             .catch((error) => {
                 console.log(error)
             })
-    }, [asc])
+    }, [asc, search])
+
+    const handleSearch = () => {
+        console.log(searchRef.current.value);
+        setSearch(searchRef.current.value);
+
+    }
 
     return (
         <div className="mt-5">
@@ -25,9 +32,19 @@ const Services = () => {
                     <br />
                     words which dont look even slightly believable</h2>
 
-                <button 
+                <div className="form-control">
+                    <div className="input-group">
+                        <input type="text" ref={searchRef}  placeholder="Search…" className="input input-bordered" />
+                        <button onClick={handleSearch} className="btn btn-square">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                </div>
+
+                <button
                     onClick={() => setAsc(!asc)}
-                    className="btn btn-primary"> Price: {asc ? "High to Low" : "Low to High"}</button>
+                    className="btn btn-primary"> Price: {asc ? "High to Low" : "Low to High"}
+                </button>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                 {
